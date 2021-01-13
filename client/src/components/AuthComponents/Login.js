640fe574d5f86835df51490eb6d2c83b458e53e9
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2021-01-12 23:51:20 -0800

    styled login form

--- client/src/components/AuthComponents/Login.js
+++ client/src/components/AuthComponents/Login.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
+
+    
+    const register = () => {}
+    const loginToApp = () => {}
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login">
+            <h1>EXPOSIUM</h1>
+            <form>
+                <input placeholder="Email" type="email" />
+
+                <input placeholder="Password***" type="password" />
+            <button type="submit" onClick={loginToApp}>Sign in</button>
+            </form>
+
+            <p>Not registered?<span className="login__register" onClick={register}>Register here</span></p>
         </div>
     )
 }
